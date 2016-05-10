d5297f0ddd0e8b7d6dec511b1c76526f88b0761f
antoinerollin <antoine.rollin26@free.fr>
2016-05-10 16:29:27 +0200

    CA MARCHE

--- j2e-dockers/run.sh
+++ j2e-dockers/run.sh
@@ -3,4 +3,4 @@ sudo docker rm j2e
 sudo docker build -t stata/j2e .
 echo ">>>>> [J2E image built with success]"
 echo ""
-sudo docker run -it -p 8080:8080 --name j2e stata/j2e
+sudo docker run -it -p 8080:8080 --net="host" --name j2e stata/j2e
